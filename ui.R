b144257001a9e96f1069ae5fb64d73ac3f7f6ae5
ninnakin <kristinajuhlin@gmail.com>
2015-12-25 18:50:42 +0100

    Added instructions to the palindrom app

--- ui.R
+++ ui.R
@@ -8,13 +8,17 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
     width=5,
-    helpText("This app identifies if a word is a palindrom or not"),
+    helpText("Type a word or sentence in the box below to see if it is a palindrom or not"),
     br(),
-    textInput("text", "Is this a palindrom?")
+    textInput("text", "Is this a palindrom?"),
+    br(),
+    textOutput("text")
     ),
     
   mainPanel(
-    textOutput("text")
+    h3("Instructions"),
+    helpText("This simple app identifies if a word is a palindrom or not. Just type the word in the box and watch the output. Special characters and blanks are ignored.")
+    
   )
     
   )
